d3b23de452eeda1d52e3858b0292e06af6c49571
Mark Lescroart <mlescroart@unr.edu>
2024-10-24 14:28:22 -0700

    Python exp1 ver00

---
+++ Experiment1/experiment1_example_plan.py
@@ -0,0 +1,85 @@
+# Make a trial order for image presentation 
+import numpy as np
+import pathlib
+
+# Experiment parameters, stipulated
+# Basics
+subjectNumber = 1
+sname = f'subject{subjectNumber:02d}_plan.npy'
+blockRepeats = 2
+blockLength = 12 # seconds
+imageDuration = 0.5 # seconds
+imageISI = 0.1 # inter-stimulus interval, seconds
+blockISI = 3 # gap between blocks, seconds
+# Block types & sub-categories (categories in alphabetical order)
+block_categories = {
+    'bodies':['limb','body'],
+    'characters':['word','number'],
+    'faces':['adult','child'],
+    'objects':['car','instrument'],
+    'places':['house','corridor'],
+    'scrambled':['scrambled']}
+block_category_names = sorted(list(block_categories.keys()))
+# Image file location (assumes all images are in same folder, as they 
+# should be if you downloaded and unzipped fLoc from here: 
+# )
+imageFolder = pathlib.Path('~/Teaching/PSY427_627/datasets/fLoc_stimuli/')
+imageFolder = imageFolder.expanduser()
+imageType = 'jpg'
+
+# Experiment parameters, computed
+# Explicit computation of these variables will make the code clearer, and
+# our life easier, below
+imagesPerBlock = blockLength / (imageDuration + imageISI)
+if imagesPerBlock % 1: # i.e., if not whole number
+    print("block length does not divide evenly into imageDuration + imageISI!")
+    # Stop here w/ error
+    1/0
+else:
+    imagesPerBlock = int(imagesPerBlock)
+nCategories = len(block_category_names)
+nBlocks = nCategories * blockRepeats
+nTrials = nBlocks * imagesPerBlock
+# Create randomized block order
+#block_order = repmat(1:nCategories, 1, blockRepeats)
+block_order = np.hstack([range(nCategories)]*2)
+# numpy shuffle, shuffles array in place (without mapping to a new variable)
+np.random.shuffle(block_order)
+
+# Get possible images for each category
+block_images = {}
+for category, subcategories in block_categories.items():
+    # Get all possible images
+    block_images[category] = []
+    for subcategory in subcategories:
+        # Get images of a particular type, starting with the sub-category string. 
+        # This is a python specific one-liner to grab all images as Pathlib paths
+        tmp_images = sorted(list(imageFolder.glob(f'{subcategory}*{imageType}')))
+        # Add these images to our list of possible images for this category
+        block_images[category].extend(tmp_images)
+
+# Create a list to hold trials. 
+# {<image to show>, <onset time>, <offset time>, <post image time>>}
+trials = [] #cell(nTrials, 5)
+current_image_time = 1 # not right away
+for blockNumber in block_order:
+    # Select n random images from category
+    category = block_category_names[blockNumber]
+    this_block_images = np.random.choice(block_images[category], imagesPerBlock, replace=False)
+    # Select a target, repeat it
+    target_idx = np.random.randint(low=1,high=imagesPerBlock)
+    this_block_images[target_idx] = this_block_images[target_idx - 1]
+    for iimage, trial_image in enumerate(this_block_images):
+        # Image, onset, offset, is_target, blockNumber,
+        is_target = iimage == target_idx
+        this_trial = [trial_image.name,
+                      current_image_time,current_image_time + imageDuration,
+                      is_target,
+                      blockNumber,
+                      ]
+        trials.append(this_trial)
+        # Increment timing
+        current_image_time += (imageDuration + imageISI)
+    current_image_time += blockISI
+
+np.save(sname, trials)
---
+++ Experiment1/experiment1_example_run.py
@@ -0,0 +1,173 @@
+# Run experiment
+
+from psychopy import visual, core, event
+import numpy as np
+import pathlib
+import datetime
+
+# Parameters
+screenFps = 60
+ifi = 1 / screenFps
+deadlinePct = 0.1 # percent of a screen refresh interval by which 
+whileLoopDelta = 0.001 # 1 ms
+# to return control in while loops
+
+imageFolder = pathlib.Path('~/Teaching/PSY427_627/datasets/fLoc_stimuli/').expanduser()
+subjectNumber = 1
+fullScreen = False # false for debugging, True to run
+screenSize = [500,500] #[1440, 900] # For mac laptop, may need to change for you! 
+imageSize = [500,500]
+textColor = [255, 255, 255]
+fixationColor = [255, 235, 0] # a nice yellow
+fixationSize = 12 # pixels
+screenColor = [128,128,128]
+
+
+# File to save
+date = datetime.datetime.now()
+datestr = date.strftime('%Y_%m_%d_%H_%M')
+sfile = f'subject{subjectNumber:02d}_{datestr}.npz'
+# Instructions
+instructions_choice = 'Please press the button you want to use to respond'
+disallowedKeys = ['escape', 'q']
+instructions = '\n'.join([
+                'In this experiment, you will see images of',
+                'faces, places, bodies, objects, and text,',
+                'along with scrambled images.'
+                '',
+                'As you watch the images go by, please press',
+                'the response key as fast as you can if you see',
+                'a repeated image.',
+                '',
+                '(Press any key to continue)'])
+
+if fullScreen:
+    screenSize = []
+# fixation
+
+# Cerate psychopy object
+
+# Skip sync tests for now (sync tests cause issues on Mac OS)
+#Screen('Preference', 'SkipSyncTests', 0)
+# Load `trials` variable
+trials = np.load(f'subject{subjectNumber:02d}_plan.npy')
+nTrials = len(trials)
+# preallocate trials
+fields = ['keyPressed', 'rt', 'rtAbsolute', 'blockType', 'blockNumber', 'trial', 'deltaTime']
+responses = dict((f, []) for f in fields)
+# try / catch around opening screen
+try:
+    # Disable keys in matlab window
+    #ListenChar(2)
+    # Open window with default settings:
+    win = visual.Window(size=screenSize, 
+                        color=(0.5,0.5,0.5),
+                        fullscr=fullScreen, 
+                        units='pix')
+    # Choose response key
+    button_choice_message = visual.TextStim(win, text=instructions_choice, color=textColor)
+    button_choice_message.draw()
+    win.flip()
+    core.wait(.2)
+    resp_key = 'escape'
+    while resp_key in disallowedKeys:
+        key_out = event.waitKeys(maxWait=20)
+        if key_out is None:
+            raise ValueError('Timed out waiting for response!')
+        if len(key_out) >= 0:
+            resp_key = key_out[0]
+    button_chosen_message = visual.TextStim(win, text=f'You chose "{resp_key}" - moving on!', color=textColor)
+    button_chosen_message.draw()
+    win.flip()
+    core.wait(1)
+    # Draw instructions 
+    instruction_message = visual.TextStim(win, text=instructions, color=textColor)
+    instruction_message.draw()
+    win.flip()
+    _ = event.waitKeys(maxWait=20)
+    # Set up fixation 
+    fixation = visual.Circle(win, radius=fixationSize,
+                          units='pix',
+                          fillColor=fixationColor,
+                          pos=(0,0))
+    # Start timing 
+    t0 = core.getTime()
+    timeFrom = t0
+    responses = []
+    for itrial, trial in enumerate(trials):
+        img_name, onsetTime, offsetTime, isTarget, blockNumber = trial
+        image_file = str(imageFolder / img_name)
+
+        # Preallocate responses
+        this_response = {}
+        this_response['rt'] = np.nan
+        this_response['rtAbsolute'] = np.nan
+        this_response['trial'] = itrial
+        this_response['blockType'] = blockNumber
+        # Draw image texture
+        img = visual.ImageStim(win, image=image_file, units='pix', size=imageSize)
+        img.draw()
+        # Draw fixation
+        fixation.draw()
+        # Use any remaining time before presentation deadline to check for response keys
+        deadline = (t0 + float(onsetTime) - deadlinePct * ifi)
+        max_wait_time = deadline - core.getTime()
+        key_out = event.waitKeys(maxWait=max_wait_time, keyList=disallowedKeys + [resp_key], timeStamped=True)
+        if key_out is not None:
+            print('')
+            print(key_out)
+            print('')
+            if key_out[0][0] in disallowedKeys:
+                raise Exception('Manual quit!')
+            else:
+                pressed, key_time = key_out[0]
+                print(f'pressed {pressed}, at {key_time})')
+        # Wait for quit key if we've already got a response
+        max_wait_time = deadline - core.getTime()
+        key_out = event.waitKeys(maxWait=max_wait_time, keyList=disallowedKeys)
+        if key_out is not None:
+            print('')
+            print(key_out)
+            print('')
+            raise Exception('Manual quit!')
+
+        flipXX = win.flip()
+        flipOnset = core.getTime() # Don't trust flipXX
+
+        # New deadline for image down
+        deadline = (t0 + float(offsetTime) - deadlinePct * ifi)
+        max_wait_time = deadline - core.getTime()
+        key_out = event.waitKeys(maxWait=max_wait_time, keyList=disallowedKeys + [resp_key], timeStamped=True)
+        if key_out is not None:
+            print('')
+            print(key_out)
+            print('')
+            if key_out[0][0] in disallowedKeys:
+                raise Exception('Manual quit!')
+            else:
+                pressed, key_time = key_out[0]
+                print(f'pressed {pressed}, at {key_time})')
+        # Wait for quit key if we've already got a response
+        max_wait_time = deadline - core.getTime()
+        key_out = event.waitKeys(maxWait=max_wait_time, keyList=disallowedKeys)
+        if key_out is not None:
+            print('')
+            print(key_out)
+            print('')
+            raise Exception('Manual quit!')
+
+        if isTarget:
+            timeFrom = flipOnset
+        fixation.draw()
+
+        flipXX2 = win.flip()
+        flipOffset = core.getTime() # Don't trust flipXX
+        print('Image time: %0.04f\n'%(flipOffset-flipOnset))
+except:
+    win.close()
+    core.quit()
+    raise
+
+
+win.close()
+core.quit()
